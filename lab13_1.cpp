729025b07e240d24b8a1e7a672988250bcbfb4b7
Pp <Pimolnat_ka@cmu.ac.th>
2024-01-02 15:51:37 +0700

    lab13_1.cpp completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -17,3 +17,41 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double x[],int y,double z[]){
+    double sum = 0,avg;
+    for(int i=0;i<y;i++){
+        sum += x[i];
+    }
+    avg = sum/y;
+    z[0] = avg;
+
+    double sum2 = 0,SD;
+    for(int i=0;i<y;i++){
+        sum2 += pow((x[i]),2);
+    }
+    SD = sqrt((sum2/y)-(pow((avg),2)));
+    z[1] = SD;
+
+    double sum3 = 1,GM;
+    for(int i=0;i<y;i++){
+        sum3 *= x[i];
+    }
+    GM = pow(sum3,1.0/y);
+    z[2] = GM;
+
+    double sum4=0,HM;
+    for(int i=0;i<y;i++){
+        sum4 += 1/x[i];
+    }
+    HM = y/sum4;
+    z[3] = HM;
+
+    double max = x[0],min = x[0];
+    for(int i=0;i<y;i++){
+        if(x[i]>max) max=x[i];
+        if(x[i]<min) min=x[i];
+    }
+    z[4] = max;
+    z[5] = min;
+}
